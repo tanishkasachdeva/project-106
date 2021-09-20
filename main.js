8384ae06120c946f8ec0839146193a82039e06ff
tanishkasachdeva <73342817+tanishkasachdeva@users.noreply.github.com>
2021-09-20 20:09:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-prediction_1="";
-prediction_2="";
 Webcam.set({
   width: 350,
   height: 300,
@@ -16,18 +14,68 @@ function take_snapshot()
     });
 }
 console.log("ml5version",ml5.version);
-classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/XLhkxIhh8/model.json",modelLoaded);
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/l9bIN3lMP/model.json",modelLoaded);
 
 function modelLoaded()
 {
     console.log("modelLoaded!");
 }
+p
+
+function check()
+{
+    img=document.getElementById('captured_image');
+    classifier.classify(img,gotResult);
+}
+
+function gotResult(error,results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+    else
+    {
+        console.log(results);
+        document.getElementById("result_gesture_name").innerHTML=results[0].label;
+        document.getElementById("result_gesture_name2").innerHTML=results[1].label;
+        prediction_1=results[0].label;
+        if(results[0].label=="amazing")
+        {
+            document.getElementById("update_gesture").innerHTML="&#128076;";
+        }
+        if(results[0].label=="best")
+        {
+            document.getElementById("update_gesture").innerHTML="&#128077;";
+        }
+        if(results[0].label=="rock")
+        {
+            document.getElementById("update_gesture").innerHTML="&#129304;";
+        }
+        if(results[0].label=="victory")
+        {
+            document.getElementById("update_gesture").innerHTML="&#9996;";
+        }
+        if(results[0].label=="clap")
+        {
+            document.getElementById("update_gesture").innerHTML="&#128079;";
+        }
+        if(results[0].label=="hi")
+        {
+            document.getElementById("update_gesture").innerHTML="&#128400;";
+        }
+        if(results[0].label=="punch")
+        {
+            document.getElementById("update_gesture").innerHTML="&#9994;";
+        }
+        speak();
+    }
+}
 
 function speak()
 {
     var synth=window.speechSynthesis;
-    speak_data_1="the first prediction is"+prediction_1;
-    speak_data_2="and the second prediction is"+prediction_2;
-    utterThis= new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
+    speak_data_1="the hand gesture is"+prediction_1;
+    var utterThis=new SpeechSynthesisUtterance(speak_data_1);
     synth.speak(utterThis);
 }
